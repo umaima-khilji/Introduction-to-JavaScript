48d923a84536b9757c3b7d5cc1a6eb1f7a1ce67a
Umaima Khilji <umaimaayoubk@gmail.com>
2023-04-09 22:41:51 +0500

    Initialzie Project

---
+++ script/app.js
@@ -0,0 +1,157 @@
+// var username =prompt("Your name is" ,"Usama")
+// console.log(username)
+
+// var variable se start nh hoga, darmiyan me spaces nh de skty
+// java case sensitive hai
+
+// var Sawaira is ko variable banana hai
+// code browser k console pr likhna ha q k phir error dhondna muskil ho jai ga
+
+// var exp =8+7 (7/9) 
+// DMAS rule k mutabik breakits use krni ha warna result glt aiga 
+// console.log(exp)
+
+// var age =+ prompt("Your age")
+// var Fage =+ prompt("Your father's age")
+// alert(age+Fage)
+
+// DAY 1 - 6 April 
+
+// alert('Hello World')
+// var username =prompt("Your Name PLease","User")
+// var user = "User"
+// var fuser = "Sawaira"
+// console.log("Hello " + user+" " +fuser)
+
+// var sawera rasheed
+// saweraRasheed camelCase
+// SaweraRasheed PascalCase
+// sawera_rasheed snake_case
+
+// console.log(2+5)
+// console.log(2-5)
+// console.log(2*5)
+// console.log(2/5)
+// console.log(2**5)
+
+// var exp = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
+// console.log(exp)
+
+// var age =+ prompt("Your age PLease")
+// var fage =+ prompt("Your father age PLease")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+
+// var zakat_percent= 8.025
+// var user_asset = prompt ("Asset Value");
+// var result= zakat_percent*user_asset
+// alert("Your zakat="+result );
+
+
+// Home work (code run from book)
+
+// chp#1
+
+// alert("Thanks for your input!");
+// var name = "Mark";
+// name = "Ace";
+// var nationality;
+// nationality = "U.S.";
+
+// var thanx = "Thanks for your input!"
+// alert(thanx);
+
+// chp#2
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// chp#3
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum)
+
+// var originalNum = 90;
+// console.log(originalNum = originalNum + 10)
+
+// var caseQty = 144;
+// alert(caseQty);
+
+// chp#5
+// alert(2 + 2);
+// var popularNumber = 12 - 24;
+// console.log(popularNumber)
+
+// var popularNumber = 3 * 12;
+// console.log(popularNumber)
+
+//  var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+//  console.log(popularNumber)
+
+// var whatsLeftOver = 10 % 3;
+// var whatsLeftOver = 9 % 3;
+// console.log(whatsLeftOver)
+
+// chp#6
+// var num = 1;
+// var newNum = num++;
+// console.log(newNum)  
+
+// var num = 1;
+// var newNum = ++num;
+// console.log(newNum)
+
+// var num = 1;
+// var newNum = num--;
+// console.log(newNum)
+
+// var num = 1;
+// var newNum = --num;
+// console.log(newNum)
+
+
+// Chp#7
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost)
+
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost)
+
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost)
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(totalCost)
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// console.log(totalCost)
+
+// resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(totalCost)
+
+// chp#8
+// var message = "Thanks for your input!";
+// alert(message);
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// alert("2 plus 2 equals " + 2 + 2);
+
+// chp#9
+// var spec = prompt("Your species?", "human");
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
